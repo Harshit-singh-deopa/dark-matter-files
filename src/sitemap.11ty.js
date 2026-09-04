70287dab95d66bf7d58783cb8ef2166e80953cad
Harshit-singh-deopa <coofri9837136539@gmail.com>
2026-09-04 23:27:52 +0530

    Update sitemap.11ty.js

--- src/sitemap.11ty.js
+++ src/sitemap.11ty.js
@@ -33,7 +33,9 @@ exports.render = function (data) {
   for (const article of articles) {
     urls.push({
       loc: `${SITE_URL}${article.url}`,
-      lastmod: "2026-08-29"
+      lastmod: article.data.date
+        ? new Date(article.data.date).toISOString().split("T")[0]
+        : undefined,
       priority: "0.7",
     });
   }
